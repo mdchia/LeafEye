1acb47767006d733c64e035c2fedcd64b4fc5cbf
Ming-Dao Chia <u5494149@anu.edu.au>
2016-11-06 23:54:17 +1100

    pixel to temp conversion function working (with unit tests!)

--- scripts/tests.py
+++ scripts/tests.py
@@ -5,4 +5,18 @@ tests.py: unit tests
 """
 
 import unittest
+import scripts.static_process as static_process
 
+class numerical_tests(unittest.TestCase):
+
+    def test_pixel_to_temp(self):
+        test_pixel_range=(0,255)
+        test_temp_range=(20,30)
+        input=100
+        expected_output=23.92157
+        actual_output=static_process.pixel_to_temp(test_temp_range,
+                                                   test_pixel_range,input)
+        self.assertAlmostEqual(actual_output, expected_output, places=5)
+
+if __name__ == '__main__':
+    unittest.main()
